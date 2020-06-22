0e50ca929bcd7a2f56bad21169e9263542e07877
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-06-22 12:37:28 -0700

    dependencies were broken, fixing

---
+++ src/test/java/com/example/spotify/SpotifyApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.spotify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyApplicationTests
+{
+
+   @Test
+   void contextLoads()
+   {
+   }
+
+}
